533a8909611ad30fa439baf85a04c5306c06720a
Yixa <1257597350@qq.com>
2024-05-24 09:10:55 +0800

    feat:args iconFamily and error when no jsonFilePath

--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"flag"
+	"log"
+	"os"
 )
 
 func main() {
 
 	// 命令行参数
-	jsonFilePath := flag.String("f", "", "path to the JSON file")
-	styleArg := flag.String("s", "tree", "style for the JSON visualization(tree in Default)")
-	//iconFamily := flag.String("i","")
+	var jsonFilePath, styleArg, iconFamily string
+	flag.StringVar(&jsonFilePath, "f", "", "path to the JSON file")
+	flag.StringVar(&styleArg, "s", "tree", "style for the JSON visualization(tree in default)")
+	flag.StringVar(&iconFamily, "i", "null", "icon family for the JSON visualization(null in default)")
 	flag.Parse()
+	if jsonFilePath == "" {
+		log.Fatalf("Error: '-f' flag is required but not provided.")
+	}
+	file, err := os.Open(jsonFilePath)
+	if err != nil {
+		log.Fatalf("Error opening JSON file: %v", err)
+	}
+	defer file.Close()
 	//fmt.Printf("f: %v; s: %v", *jsonFilePath, *styleArg)
 }
