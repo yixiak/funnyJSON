d74a5d3d4972c53b9e79624074761170060ee14c
Yixa <1257597350@qq.com>
2024-05-24 13:26:01 +0800

    wip:drawJSON interface and drawer struct

---
+++ drawer/drawer.go
@@ -0,0 +1,7 @@
+package drawer
+
+type drawer struct {
+}
+
+// ParseJSON 为 drawer 实现 drawJSON 接口
+func (d *drawer) ParseJSON(filename string) {}
---
+++ drawer/interface.go
@@ -0,0 +1,20 @@
+package drawer
+
+// 使用生成器模式，可以自定义配置
+type drawJSON interface {
+	// ParseJSON
+	// 根据文件名解析 json
+	ParseJSON(jsonfile string) error
+
+	// InitStyle
+	// 选择对应的风格
+	InitStyle(style string)
+
+	// InitIcon
+	// 选择对应的图标族
+	InitIcon(IconFamily string)
+
+	// Draw
+	// 最后输出
+	Draw()
+}
