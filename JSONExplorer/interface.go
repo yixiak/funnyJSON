b46b04452bacace80ab289591bc80ef3f420da6e
Yixa <1257597350@qq.com>
2024-05-25 21:45:15 +0800

    wip: style family interface

--- JSONExplorer/interface.go
+++ JSONExplorer/interface.go
@@ -32,3 +32,32 @@ type IconFactory interface {
 type drawJSON interface {
 	Draw()
 }
+
+type StyleFamily interface {
+	Get_symbol_last() string      // 最后一个子对象的前缀
+	Get_symbol() string           // 正常子对象的前缀
+	Get_symbol_left_last() string // 左下角
+
+	// prefix
+	// 实际上指selfjson的前缀
+	Get_prefix_end_g() string   // 全局的最后一个节点，一定在最底部
+	Get_prefix_endleaf() string //前json对象的最后一个节点，但下面还有其他的
+	Get_prefix() string
+
+	// selfjson
+	// 对应icon+key+value
+	Get_leafprefix() string
+	Get_leaftendconprefix() string
+	Get_leafend() string
+	Get_conend() string
+
+	// end
+	Get_end() string       // 中间填充
+	Get_end_first() string // 右上角
+	Get_end_mid() string   // 右边框
+	Get_end_last() string  // 右下角
+}
+
+type StyleFactory interface {
+	CreateStlyeFamily() StyleFamily
+}
